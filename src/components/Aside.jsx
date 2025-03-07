35bb750b8ab6800aefa2a09689f0f646d6d74af3
yhonder710 <agueropibe1@gmail.com>
2025-03-07 09:38:53 -0400

    reformacion

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,14 +1,14 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
+import { faCircleUser, faGem, faAddressBook, faStar } from '@fortawesome/free-solid-svg-icons';
 
 export function Aside () {
   return (
     <>
-    <aside className="bg-[#516e4f] w-10 sm:w-20 h-full px-5 py-3 text-left flex justify-between flex-col fixed ">
-      <section className="bg-amber-50">logo</section>
-      <section className="bg-amber-50">Skill</section>
-      <section className="bg-amber-50 flex flex-col-reverse">Contacts <span className="block"><FontAwesomeIcon icon={faCircleUser} /></span></section>
-      <section className="bg-amber-50">Projects</section>
+    <aside className="bg-gray-900 w-10 sm:w-20 h-full px-5 py-10 text-left flex justify-between items-center flex-col fixed border-r-[1px] border-[#f5f5f5] shadow-2xs shadow-black">
+      <section className=""><FontAwesomeIcon className='text-4xl text-white' icon={faCircleUser} /></section>
+      <section className=""><FontAwesomeIcon className='text-4xl text-white' icon={faStar} /></section>
+      <section className=""><FontAwesomeIcon className='text-4xl text-white' icon={faAddressBook} /></section>
+      <section className=""><FontAwesomeIcon className='text-4xl text-white' icon={faGem} /></section>
     </aside>
     </>
   )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 export function Main (){
   return(
-    <main className="bg-[#91AC8F] w-full  pl-40 py-5">
+    <main className="bg-gray-800 w-full  pl-40 py-5">
     </main>
   )
 }
