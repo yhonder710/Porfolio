2009cf4fcf28232790a8525a493d9a3d4a941893
yhonder710 <agueropibe1@gmail.com>
2025-03-07 09:38:53 -0400

    reformacion

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -8,19 +8,9 @@ export function Aside () {
 
   return (
     <>
-<<<<<<< HEAD
-    <aside className="bg-[#516e4f] w-15 sm:w-20 h-full px-3 py-3 text-left flex justify-between flex-col fixed ">
-      <section className="bg-amber-50">logo</section>
-      <section className="bg-amber-50">Skill</section>
-      <section className="bg-amber-50 flex flex-col-reverse"><FontAwesomeIcon icon={faCircleUser} /></section>
-      <section className="bg-amber-50">Projects</section>
-=======
-    <aside className="bg-gray-900 w-10 sm:w-20 h-full px-5 py-10 text-left flex justify-between items-center flex-col fixed border-r-[1px] border-[#f5f5f5] shadow-2xs shadow-black">
-      <section className=""><FontAwesomeIcon className='text-4xl text-white' icon={faCircleUser} /></section>
-      <section className=""><FontAwesomeIcon className='text-4xl text-white' icon={faStar} /></section>
-      <section className=""><FontAwesomeIcon className='text-4xl text-white' icon={faAddressBook} /></section>
-      <section className=""><FontAwesomeIcon className='text-4xl text-white' icon={faGem} /></section>
->>>>>>> 35bb750 (reformacion)
+    <aside className={sidebar}>
+      <button className="bg-gray-950 w-12 h-12 text-2xl absolute right-[-15px] top-[50%] font-bold text-white cursor-pointer rounded-2xl border-2 border-white" onClick={() => setOpen(!open)}>🡢</button>
     </aside>
+    </>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,7 @@
 export function Main (){
   return(
-<<<<<<< HEAD
-    <main className="bg-[#91AC8F] w-full  pl-35 py-5">
-=======
-    <main className="bg-gray-800 w-full  pl-40 py-5">
->>>>>>> 35bb750 (reformacion)
+    <main className="bg-gray-800 w-full h-full pl-30">
+
     </main>
   )
 }
