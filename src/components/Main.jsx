387056fe897f19db0b8b57276c43da7b1819c024
yhonder710 <agueropibe1@gmail.com>
2025-03-07 09:38:53 -0400

    reformacion

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,10 @@
 export function Main (){
   return(
+<<<<<<< HEAD
     <main className="bg-[#91AC8F] w-full  pl-35 py-5">
+=======
+    <main className="bg-gray-800 w-full  pl-40 py-5">
+>>>>>>> 35bb750 (reformacion)
     </main>
   )
 }
