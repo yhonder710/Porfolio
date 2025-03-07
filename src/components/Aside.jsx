6c90ee4c41dbf1da149f3307bc46f77ef9c8b3ea
yhonder710 <agueropibe1@gmail.com>
2025-03-07 18:36:16 -0400

    listo

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -7,8 +7,10 @@ export function Aside () {
   const sidebar = open ? 'open on' : 'open'
 
   return (
+    <>
     <aside className={sidebar}>
       <button className="bg-gray-950 w-12 h-12 text-2xl absolute right-[-15px] top-[50%] font-bold text-white cursor-pointer rounded-2xl border-2 border-white" onClick={() => setOpen(!open)}>🡢</button>
     </aside>
+    </>
   )
 }
