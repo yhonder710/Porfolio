5f52cb99fd2edee4f632f21bfd461a4f8409f357
yhonder710 <agueropibe1@gmail.com>
2025-03-07 21:45:46 -0400

    funcion del menu lista

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 import { useState } from "react"
 
 
@@ -5,11 +7,29 @@ export function Aside () {
   const [open, setOpen] = useState(false)
 
   const sidebar = open ? 'open on' : 'open'
+  const btn = open ? '🡠' : '🡢'
+  const link = open ? 'link on' : 'link'
 
   return (
     <>
     <aside className={sidebar}>
-      <button className="bg-gray-950 w-12 h-12 text-2xl absolute right-[-15px] top-[50%] font-bold text-white cursor-pointer rounded-2xl border-2 border-white" onClick={() => setOpen(!open)}>🡢</button>
+      <nav className="h-screen flex text-white w-full justify-center">
+        <ul className="flex flex-col justify-around items-center text-5xl">
+          <li className='flex gap-5'><FontAwesomeIcon icon={faCircleUser} />
+            <span className={link}>logo</span>
+          </li>
+          <li className='flex gap-5'><FontAwesomeIcon icon={faCircleUser} />
+            <span className={link}>logo</span>
+          </li>
+          <li className='flex gap-5'><FontAwesomeIcon icon={faCircleUser} />
+           <span className={link}>logo</span>
+          </li>
+          <li className='flex gap-5'><FontAwesomeIcon icon={faCircleUser} />
+           <span className={link}>logo</span>
+          </li>
+        </ul>
+      </nav>
+      <button className="bg-gray-950 w-12 h-12 text-2xl absolute right-[-15px] top-[50%] font-bold text-white cursor-pointer rounded-2xl border-2 border-white" onClick={() => setOpen(!open)}>{btn}</button>
     </aside>
     </>
   )
