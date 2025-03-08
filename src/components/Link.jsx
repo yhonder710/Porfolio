50d110f80b2cc3b93249c8d3f774c3c7f6a724c6
yhonder710 <agueropibe1@gmail.com>
2025-03-08 12:55:45 -0400

    cambio en el hover

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,7 +1,7 @@
 
 export function Link ({ link, icono, text }) {
   return (
-    <li className='flex gap-5 hover:text-gray-300 cursor-pointer'> {icono}
+    <li className='flex gap-5 hover:text-gray-400 cursor-pointer'> {icono}
         <a href="#" className={link}>{text}</a>
       </li>
   )
