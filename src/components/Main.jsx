ccb340a6f444f19a776084e3f775f41ae41c7e40
kicknash01 <olivercordero0000@gmail.com>
2025-03-08 21:56:00 -0400

    empezando inicio

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,17 @@
 export function Main (){
   return(
-    <main className="bg-gray-800 w-full h-full pl-30">
-
+    <main className="bg-gray-800 w-full h-full pl-30 flex flex-col justify-center items-center gap-10 py-5">
+      <div className="w-250 mt-20">
+        <h2 className="text-[40px]">
+          Es un placer, Soy Yhonder
+          </h2>
+        <h3 className="text-[22px]">
+          Desarrolador Full Stack Autodidactica
+          </h3>
+      </div>
+      <div className="w-250">
+        <p className="text-[45px]">Contactanos</p>
+      </div>
     </main>
   )
 }
